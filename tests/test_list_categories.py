767a7707a14c7fe2036283c257957828f98c7347
DK <zaf451335@gmail.com>
2025-08-22 03:08:44 +0200

    Delete tests directory

--- tests/test_list_categories.py
+++
@@ -1,8 +0,0 @@
-import os
-import utils
-
-
-def test_list_categories_includes_fleet():
-    utils.DOSSIERS_DIR = os.path.join(utils.BASE_DIR, "dossiers")
-    categories = utils.list_categories()
-    assert "fleet" in categories
